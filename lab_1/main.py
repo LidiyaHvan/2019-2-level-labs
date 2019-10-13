21c16329c4d130734a323a456a3a3c46dda9baa3
LidiyaHvan <lidiya.200062@gmail.com>
2019-10-13 17:17:13 +0300

    Final ^_^

--- lab_1/main.py
+++ lab_1/main.py
@@ -33,69 +33,60 @@ def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    frequencies = {}
-    if text == None:
-        return frequencies
-    else:
-        if type(text) is int:
-            return frequencies
-        elif len(text) == 0:
-            return frequencies
+    if text is None or text == '':
+        return {}
+    if isinstance(text, int):
+        return {}
+    clear_text = ''
+    for word in text:
+        if word.isalpha():
+            clear_text += word
+        elif word == ' ' or word == '\n':
+            clear_text += word
         else:
-            splitted = (text.replace('\n', ' ')).split(' ')
-            prohibited_marks = ['-', '.', '!', ',', '?', '"', "'", ':', ';', '/', '*', '%', '$', '(', ')', '_']
-            res = []
-            for word in splitted:
-                if not word.isdigit() and word not in prohibited_marks:
-                    clear_word = ''
-                    for elem in word:
-                        if elem not in prohibited_marks:
-                            clear_word += elem
-                    if clear_word is not '':
-                        res.append(clear_word)
-            for key in res:
-                if key in frequencies:
-                    frequencies[key] += 1
-                else:
-                    frequencies[key] = 1
-    return frequencies
-stop_words = ('ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about',
-              'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be',
-              'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself',
-              'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
-              'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through',
-              'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should',
-              'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all',
-              'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in',
-              'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
-              'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has',
-              'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few',
-              'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing',
-              'it', 'how', 'further', 'was', 'here', 'than')
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
+            clear_text += ' '
+    clear_text = clear_text.lower()
+    splitted_text = clear_text.split()
+    freq_dict = {}
+    for new_word in splitted_text:
+        number_of_words = splitted_text.count(new_word)
+        freq_dict[new_word] = number_of_words
+    return freq_dict
+
+def filter_stop_words(freq_dict: dict, stop_words: tuple) -> dict:
     """
     Removes all stop words from the given frequencies dictionary
     """
-    filtered_dict = {}
-    if frequencies is None or len(frequencies) == 0:
-        filtered_dict = {}
-        return filtered_dict
-    else:
-        for key, value in frequencies.items():
-            if key not in stop_words or isinstance(key, str):
-                filtered_dict[key] = value
+    if freq_dict is None and stop_words is None:
+        return {}
+    elif freq_dict is None and stop_words is not None:
+        return {}
+    elif freq_dict is not None and stop_words is None:
+        return freq_dict
+    filtered_dict = freq_dict.copy()
+    for key in freq_dict:
+        if not isinstance(key, str):
+            del filtered_dict[key]
+    for stop in stop_words:
+        if stop in filtered_dict:
+            del filtered_dict[stop]
     return filtered_dict
 
-top_n = 4
 
 def get_top_n(filtered_dict: dict, top_n: int) -> tuple:
     """
     Takes first N popular words
     """
-    if filtered_dict == {} or top_n <= 0:
-        return ()
-    elif top_n > len(filtered_dict):
-        top_n = len(filtered_dict)
-    top_list = sorted(filtered_dict.items(), key=lambda n: n[1], reverse=True)
-    done = tuple(top_list[:top_n])
-    return done
+    if top_n > 0:
+        list_of_words = list(filtered_dict.items())
+        list_of_words.sort(key=lambda el_value: -el_value[1])
+        top_words = []
+        for elm in list_of_words:
+            first_elm = elm[0]
+            top_words.append(first_elm)
+        fin_tuple = tuple(top_words[:top_n])
+    else:
+        fin_tuple = ()
+    return fin_tuple
+
+
